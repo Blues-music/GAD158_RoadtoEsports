13f60cc700b88a2462c74fa5ccdae09a7b8b09e9
Blues-music <jameschamberes@gmail.com>
2018-10-31 11:31:26 +1100

    Added Movement and Some colour
    Added the movement script and work-in-progress colours

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Character's move speed value
+    public float moveSpeed;
+    // Bool to only allow 4 directional movement
+    bool moveX;
+    bool moveY;
+
+    // Initialising attributes
+    public int str;
+    public int agi;
+    public int dex;
+    public int iq;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Code that allows player to move character
+        if ( Input.GetAxisRaw("Horizontal") > 0.2f || Input.GetAxisRaw("Horizontal") < -0.2f)
+        {
+            transform.Translate(new Vector3((Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime), 0f, 0f));
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.2f || Input.GetAxisRaw("Vertical") < -0.2f)
+        {
+            transform.Translate(new Vector3(0f, (Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime), 0f));
+        }
+    }
+}
