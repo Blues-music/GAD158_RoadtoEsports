db9d741d0eda574aa1f9a8c23563cc66598ab3bb
Blues-music <jameschamberes@gmail.com>
2018-11-18 16:26:20 +1100

    Interaction with pc + other things
    Interacting with pc now calls minigame scene. Animations are better. Sharped images of the sprites.

---
+++ Assets/Scripts/PCToAccessMinigame.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PCToAccessMinigame : MonoBehaviour {
+
+    public GameObject text;
+
+    bool playerEntered;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerEntered == true && Input.GetButtonDown("Interact"))
+        {
+            Debug.Log("hit");
+            SceneManager.LoadScene("Mini-Game");
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerEntered = true;
+        text.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        playerEntered = false;
+        text.SetActive(false);
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,6 +10,10 @@ public class PlayerScript : MonoBehaviour {
     bool moveX;
     bool moveY;
 
+    //
+    private bool playerMoving;
+    private Vector2 lastMove;
+
     // Initialising animator
     private Animator animator;
 
@@ -22,7 +26,6 @@ public class PlayerScript : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-
         animator = GetComponent<Animator>();
 
         str = 0;
@@ -34,20 +37,29 @@ public class PlayerScript : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        playerMoving = false;
 
         // Code that allows player to move character
+        // 
         if (Input.GetAxisRaw("Horizontal") > 0.2f || Input.GetAxisRaw("Horizontal") < -0.2f)
         {
             transform.Translate(new Vector3((Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime), 0f, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.2f || Input.GetAxisRaw("Vertical") < -0.2f)
         {
             transform.Translate(new Vector3(0f, (Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime), 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
         }
 
         // Code to set the animations
         animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        animator.SetBool("PlayerMoving", playerMoving);
+        animator.SetFloat("LastMoveX", lastMove.x);
+        animator.SetFloat("LastMoveY", lastMove.y);
     }
 }
