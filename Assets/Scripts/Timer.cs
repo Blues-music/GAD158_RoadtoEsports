d8cdc3c5f867267163d9392f21096564e887bd6f
Morris <morrisxlee@gmail.com>
2018-11-19 11:08:42 +1100

    Timer
    Added a timer in mini game. When The timer hits 0, go back to the Room scene.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+    //Count down time start
+    float timeRemaining = 20;
+    public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeRemaining -= Time.deltaTime;
+	}
+
+    
+    void OnGUI()
+    {
+        if(timeRemaining > 0)
+        {
+            //The position of the timer
+            //GUI.Label(new Rect(1300, 0, 200, 100), "Time: " + (int)timeRemaining);
+            timerText.text = ("Time: " + (int)timeRemaining) ;
+        }
+        else
+        {
+            //When the time is 0, load scene(room)
+            SceneManager.LoadScene(1);
+        }
+    }
+}
